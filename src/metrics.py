918fa20d53e7abbaa1306dd6d4fb99ddc21d72ae
Marcus Backstrom <VBMarcusVB@hotmail.com>
2025-11-12 13:13:16 +0100

    got per gender and total summery

--- src/metrics.py
+++ src/metrics.py
@@ -3,8 +3,9 @@ import pandas as pd
 # age,sex,height,weight,systolic_bp,cholesterol
 
 def summery_of_csv(df: pd.DataFrame) -> pd.DataFrame:
-
-    summery_of_csv = df["age", "height", "weight", "systolic_bp", "cholesterol" ].agg(["sum", "mean", "median", "std", "min", "max"])
+    
+    columns = ["age", "height", "weight", "systolic_bp", "cholesterol"]
+    summery_of_csv = df[columns].agg(["sum", "mean", "median", "std", "min", "max"])
     
     return summery_of_csv
 
@@ -12,6 +13,6 @@ def summery_of_csv_per_gender(df: pd.DataFrame) -> pd.DataFrame:
     
     columns = ["age", "height", "weight", "systolic_bp", "cholesterol"]
     
-    summery_per_gender = df.groupby("sex")[columns].agg(["sum", "mean", "median", "std", "min", "max"])
+    summery_per_gender = df.groupby("sex")[columns].agg(["mean", "median", "std", "min", "max"])
     
     return summery_per_gender
