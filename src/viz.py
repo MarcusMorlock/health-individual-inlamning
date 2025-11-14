eedbc400db34c3d05d7684a69544cca0a13629ea
Marcus Backstrom <VBMarcusVB@hotmail.com>
2025-11-14 13:19:46 +0100

    first Graf of smokers in viz.py

--- src/viz.py
+++ src/viz.py
@@ -1,12 +1,32 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from .metrics import (summery_of_csv,)
+import numpy as np
+from .metrics import (summery_of_csv,smoker_per_gender)
 
 
 def viz_summery(df :pd.DataFrame):
     return NotImplemented
 
 def viz_smoker_per_gender(df :pd.DataFrame) -> plt.subplots:
-    fig, ax = plt.subplots(figsize=(8, 6))
+    smokers = smoker_per_gender(df)
 
-    
+    Sexes = smokers.index
+    Total = smokers["Total"]
+    Smokers = smokers["Smokers"]
+
+    x = np.arange(len(Sexes))
+    width = 0.80
+
+    fig, ax = plt.subplots(figsize=(6,5))
+
+    ax.bar(x, Total, width=width, label="Total")
+    ax.bar(x, Smokers, width=width, label="Smokers")
+    ax.set_title("Smoker per Gender")
+    ax.set_xticks(x)
+    ax.set_xticklabels(Sexes)
+    ax.grid(True, axis="y", alpha=0.3, color="black")
+    ax.legend()
+
+    plt.tight_layout()
+
+
